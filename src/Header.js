633c4047bcd83a23e7a5795592224e13bfbd48e4
skaum <skaum103@outlook.com>
2024-03-06 15:32:11 -0400

    dependency issue

--- src/Header.js
+++ src/Header.js
@@ -7,7 +7,7 @@ function Header() {
         <nav class="pg-main-nav">
           <ul>
               <li><a href="/">Home</a></li>
-              <li><a href="https://www.google.ca" target="_blank" rel="noopener">Search on Google</a></li>
+              <li><a href="https://www.google.ca" target="_blank" rel="noreferrer">Search on Google</a></li>
               <li><a href="mailto:email@test.ca">Email Us</a></li>
           </ul>
         </nav>
